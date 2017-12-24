156bbde4e737c8cd640556e128a5c3d062041d7e
aleksacastle <aaklochko@gmail.com>
2017-12-24 19:21:55 +0200

    added routes for ember todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #route for vue application
   resources :todos, except: %i[:new :edit]
   root 'todos#index'
+
+  #route for ember application
+  namespace :ember do
+    resources :todos, except: %i[:new :edit]
+  end
 end
