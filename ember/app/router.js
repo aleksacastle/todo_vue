e223eb54b6a797e8d64f86af55b83a17212ee377
aleksacastle <aaklochko@gmail.com>
2017-12-31 23:58:11 +0200

    changed router and index layout

--- ember/app/router.js
+++ ember/app/router.js
@@ -1,12 +1,14 @@
-import EmberRouter from '@ember/routing/router';
+import Ember from 'ember';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+const Router = Ember.Router.extend({
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
+	this.route('active');
+	this.route('completed');
 });
 
 export default Router;
