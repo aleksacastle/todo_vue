a15a5ed075515929f9577558f5cb88e3625582c6
aleksacastle <aaklochko@gmail.com>
2018-01-02 21:38:07 +0200

    start ember through rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   #route for vue application
     resources :todos, except: %i[:new :edit]
-    root 'todos#index'
+    # root 'todos#index'
 
     #route for ember application
     namespace :ember do
       resources :todos, except: %i[:new :edit]
     end
 
-    mount_ember_app :ember, to: "ember/todos"
+    mount_ember_app :ember, to: "/"
 end
