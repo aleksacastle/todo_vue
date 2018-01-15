be4fa3f27b2551cc6ea635ffbf8befa2e3165e13
aleksacastle <aaklochko@gmail.com>
2018-01-15 23:08:48 +0200

    create component my navigation
    add my navigatopm html
    
    add my navigation css
    
    try to make it work

---
+++ app/assets/components/my-navigation/my-navigation.js
@@ -0,0 +1,7 @@
+class MyNavigation extends Polymer.Element {
+  static get is() { return 'my-navigation';
+console.log('Navigation initialized'); }
+
+}
+
+window.customElements.define("my-navigation", MyNavigation )
