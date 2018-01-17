e48b727e0e6dd13e88e69cba44faf04a9ad14724
aleksacastle <aaklochko@gmail.com>
2018-01-18 00:23:43 +0200

    polymer component that works

--- app/assets/components/my-navigation/my-navigation.js
+++
@@ -1,7 +0,0 @@
-class MyNavigation extends Polymer.Element {
-  static get is() { return 'my-navigation';
-console.log('Navigation initialized'); }
-
-}
-
-window.customElements.define("my-navigation", MyNavigation )
